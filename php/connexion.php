836471ee18a8af84a41773c75c7c0243f66b7e71
Salma <dreambellytb@gmail.com>
2025-04-27 15:12:10 +0200

    creations des fichiers php

---
+++ php/connexion.php
@@ -0,0 +1,11 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "simpletask";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+  die("Connexion échouée: " . $conn->connect_error);
+}
